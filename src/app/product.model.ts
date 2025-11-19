4819ccb6879c227de52d66e45d790ed02184fec4
Samuel1w <111092737+Samuel1w@users.noreply.github.com>
2025-11-19 09:43:42 +0100

    Update product.model.ts

--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -2,7 +2,13 @@ export interface Product {
   id:number;
   title:string;
   price:number;
-  thumbnail:string;
+  thumbnail: string | CloudinaryImage;
+  subimages: (string | CloudinaryImage)[];
   sold?:string;
   store?:string;
+}
+
+export interface CloudinaryImage {
+  secure_url?: string;
+  url?: string;
 }
